d94704815f3438d92743645f69d5c4bb5eb8bfb6
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-06-30 00:27:49 +0200

    Implemented Firebase auth service
    Implemented login and user in Firebase app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,6 +45,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.google.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -51,4 +53,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(platform(libs.firebase.bom))
 }
--- app/src/main/java/com/visualstudioex3/logincompose/MainActivity.kt
+++ app/src/main/java/com/visualstudioex3/logincompose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,21 +27,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LoginScreen() {
     val context: Context = LocalContext.current
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     var usuario: String by remember { mutableStateOf("") }
     var password: String by remember { mutableStateOf("") }
 
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
         Column(
-            modifier = Modifier.padding(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Inicio de Sesión",
-                style = MaterialTheme.typography.headlineMedium
+                text = "Inicio de Sesión", style = MaterialTheme.typography.headlineMedium
             )
 
             Spacer(modifier = Modifier.height(20.dp))
@@ -48,8 +47,7 @@ fun LoginScreen() {
             OutlinedTextField(
                 value = usuario,
                 onValueChange = { usuario = it },
-                label = { Text("Usuario") }
-            )
+                label = { Text("Usuario") })
 
             Spacer(modifier = Modifier.height(10.dp))
 
@@ -64,19 +62,30 @@ fun LoginScreen() {
 
             Button(
                 onClick = {
-                    Toast.makeText(
-                        context,
-                        if (usuario == "admin" && password == "1234")
-                            "Acceso Correcto"
-                        else
-                            "Usuario o contraseña incorrectos",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                },
-                Modifier.defaultMinSize(minWidth = OutlinedTextFieldDefaults.MinWidth)
+                    if (usuario.isEmpty() || password.isEmpty())
+                        showToast(
+                            context,
+                            "Debe introducir un valor para el usuario y para el password."
+                        )
+                    else auth.signInWithEmailAndPassword(usuario, password)
+                        .addOnCompleteListener {
+                            showToast(
+                                context,
+                                if (it.isSuccessful)
+                                    "Acceso Correcto"
+                                else
+                                    "Usuario o contraseña incorrectos"
+                            )
+                        }
+                }, Modifier.defaultMinSize(minWidth = OutlinedTextFieldDefaults.MinWidth)
             ) {
                 Text("Ingresar")
             }
         }
     }
+}
+
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT)
+        .show()
 }
